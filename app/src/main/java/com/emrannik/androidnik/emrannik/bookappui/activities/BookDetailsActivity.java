46fb935ec87c2da3cd3c15f8877d79d3fd807df3
emran13 <emran.nannu@gmail.com>
2020-12-07 15:22:05 +0600

    Initial commit

---
+++ app/src/main/java/com/emrannik/androidnik/emrannik/bookappui/activities/BookDetailsActivity.java
@@ -0,0 +1,42 @@
+package com.emrannik.androidnik.emrannik.bookappui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import com.bumptech.glide.Glide;
+import com.emrannik.androidnik.emrannik.bookappui.Book;
+import com.emrannik.androidnik.emrannik.bookappui.R;
+
+public class BookDetailsActivity extends AppCompatActivity {
+
+    ImageView imgbook;
+    RatingBar ratingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_details);
+
+        //ini view
+        imgbook = findViewById(R.id.item_book_img);
+        ratingBar = findViewById(R.id.item_book_ratingbar);
+
+        // we need to get book item object
+
+        Book item = (Book) getIntent().getExtras().getSerializable("bookObject");
+
+        loadBookData(item);
+
+
+    }
+
+    private void loadBookData(Book item) {
+
+        // bind book data here for now i will only load the book cover image
+
+        Glide.with(this).load(item.getDrawableResource()).into(imgbook);
+
+    }
+}
